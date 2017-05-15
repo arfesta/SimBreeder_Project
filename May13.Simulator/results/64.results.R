c24adb1a602f12c664b4bae85fd241161e2bea70
arfesta <arfesta@ncsu.edu>
2017-05-15 18:39:56 -0400

    added new results

---
+++ May13.Simulator/results/64.results.R
@@ -0,0 +1,32 @@
+the.files <- list.files("./64/AAA",full.names = T)
+lapply(the.files,load,.GlobalEnv)
+rm(the.files)
+
+AAA <- lapply(ls(), get)
+rm(list=setdiff(ls(), "AAA"))
+
+the.files <- list.files("./64/AGG",full.names = T)
+lapply(the.files,load,.GlobalEnv)
+rm(the.files)
+
+AGA <- lapply(setdiff(ls(),"AAA"), get)
+rm(list=setdiff(ls(), c("AAA","AGA")))
+
+
+for(x in 1:length(AGA)) {
+  this <- AGA[[x]]$g
+  if (x==1) { AGA.g <- as.matrix(this)} else { AGA.g <- rbind(AGA.g,this)} 
+  
+} ; rm(this,x)
+AGA.g = apply(AGA.g,2,mean)
+
+
+for(x in 1:length(AAA)) {
+  this <- (AAA[[x]]$g)
+  if (x==1) { AAA.g <- as.matrix(this)} else { AAA.g <- rbind(AAA.g,this)} 
+  
+} ; rm(this,x)
+AAA.g = apply(AAA.g,2,mean)
+
+plot(1:10,AGA.g,ylim=c(min(AAA.g),max(AGA.g)))
+lines(1:10,AAA.g)
